93a77434af5c395f62333af8f5f9420f76937e82
Thorsten Reisinger <52202041@edu.campus02.at>
2022-09-16 19:12:46 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+   //main + TAB
+   //psvm + TAB
+    public static void main(String[] args) {
+        //sout + TAB
+        System.out.println("Hello World!");
+        System.out.println("Welcome to my world");
+        System.out.println("Zeile 3"); //STRG + D = kopieren von Zeilen
+        System.out.println("Zeile 4");
+    }
+}
